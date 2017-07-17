c511a79494417347ef08a6e470ae8dbca836e332
Giuseppe Scrivano <gscrivan@redhat.com>
2017-07-18 00:41:57 +0200

    net-snmp: new container
    Signed-off-by: Giuseppe Scrivano <gscrivan@redhat.com>

---
+++ net-snmp/run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+(
+    . /etc/sysconfig/snmpd
+    if test "$WAIT_FOR_CONFIGURATION" == 1; then
+        systemd-notify --ready
+        exec sleep 9999d
+    fi
+)
+
+(
+    OPTIONS="-Lsd"
+    . /etc/sysconfig/snmptrapd
+    unset NOTIFY_SOCKET
+    /usr/sbin/snmptrapd $OPTIONS -f
+) &
+
+OPTIONS="-LS0-6d"
+. /etc/sysconfig/snmpd
+exec /usr/sbin/snmpd $OPTIONS -f
